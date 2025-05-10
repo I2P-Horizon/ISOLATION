5eb8e3239020a2c470265cf0baa55a40491a39bf
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-05-10 14:29:44 +0900

    환경 설정 시스템
    버튼 컨트롤러 class
    - 선택된 버튼에만 체크마크 표시.
    
    해상도 컨트롤러 class
    - 사용 가능한 해상도 중 허용된 해상도만 필터링 -> Dropdown에 표시.
    
    그래픽 컨트롤러 class
    - 전달된 index값에 따라 그래픽 품질 레벨 변경.

--- Assets/KSW/Script/GameSettings.cs
+++ Assets/KSW/Script/GameSettings.cs
@@ -1,23 +1,157 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameSettings : MonoBehaviour
+#region 버튼 컨트롤러
+[System.Serializable]
+public class ButtonController
 {
-    [SerializeField] private GameObject[] screenCheckmarks; // 화면 설정 버튼 배열
-    [SerializeField] private GameObject[] graphicsCheckmarks; // 그래픽 설정 버튼 배열
-
-    private int currentScreenIndex = 0; // 화면 설정 현재 인덱스 값
-    private int currentGraphicsIndex = 0; // 그래픽 설정 현재 인덱스 값
+    [SerializeField] private GameObject[] checkmarks;
+    private int currentIndex = 0;
 
     /// <summary>
     /// 선택된 항목만 체크 표시
     /// </summary>
     /// <param name="index"></param>
-    private void SetActiveOnly(GameObject[] checkmarks, int index)
+    private void SetActiveOnly(int index)
     {
         for (int i = 0; i < checkmarks.Length; i++) checkmarks[i].SetActive(i == index);
+    }
+
+    /// <summary>
+    /// 중복 클릭 방지, 해당 인덱스의 체크마크를 활성화
+    /// </summary>
+    /// <param name="index"></param>
+    public void OnClickSetting(int index)
+    {
+        if (index == currentIndex) return;
+        SetActiveOnly(index);
+        currentIndex = index;
+    }
+
+    /// <summary>
+    /// 기본 선택 인덱스를 기준으로 체크마크 초기화
+    /// </summary>
+    /// <param name="defaultIndex"></param>
+    public void Init(int defaultIndex = 0)
+    {
+        currentIndex = defaultIndex;
+        SetActiveOnly(currentIndex);
+    }
+}
+#endregion
+
+#region 해상도 컨트롤러
+public class ResolutionController
+{
+    public Dropdown resolutionDropdown;
+
+    List<Resolution> resolutions = new List<Resolution>();
+
+    int resolutionNum;
+    int optionNum = 0;
+
+    int[,] allowedResolutions = new int[,]
+    {
+        {1920, 1080},
+        {1600, 900},
+        {1366, 768},
+        {1280, 720},
+    };
+
+    public void InitUI()
+    {
+        var filteredResolutions = Screen.resolutions
+            .Where(res => IsAllowedResolution(res.width, res.height))
+            .GroupBy(res => new { res.width, res.height })
+            .Select(g => g.First())
+            .OrderByDescending(res => res.width * res.height)
+            .ToList();
+
+        resolutions = filteredResolutions;
+        resolutionDropdown.options.Clear();
+        optionNum = 0;
+
+        foreach (Resolution item in resolutions)
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = item.width + "x" + item.height + " ";
+            resolutionDropdown.options.Add(option);
+            if (item.width == Screen.width && item.height == Screen.height) resolutionDropdown.value = optionNum;
+            optionNum++;
+        }
+
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    bool IsAllowedResolution(int width, int height)
+    {
+        for (int i = 0; i < allowedResolutions.GetLength(0); i++) if (allowedResolutions[i, 0] == width && allowedResolutions[i, 1] == height) return true;
+        return false;
+    }
+
+    public void DropdownOptionChange(int x)
+    {
+        resolutionNum = x;
+        var selected = resolutions[resolutionNum];
+        Screen.SetResolution(selected.width, selected.height, FullScreenMode.Windowed);
+    }
+}
+#endregion
+
+#region 화면 컨트롤러
+public class ScreenModeController
+{
+    private FullScreenMode currentMode;
+
+    public ScreenModeController() { currentMode = Screen.fullScreenMode; }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        currentMode = isFullScreen ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed;
+        Screen.fullScreenMode = currentMode;
+    }
+}
+#endregion
+
+#region 그래픽 컨트롤러
+public class GraphicsController
+{
+    private int currentQuality;
+
+    public void SetQuality(int index)
+    {
+        currentQuality = index;
+        QualitySettings.SetQualityLevel(currentQuality, true);
+    }
+
+    public void Init(int defaultIndex = 0)
+    {
+        currentQuality = defaultIndex;
+        QualitySettings.SetQualityLevel(currentQuality, true);
+    }
+}
+#endregion
+
+public class GameSettings : MonoBehaviour
+{
+    [SerializeField] private ButtonController screenSettings;
+    [SerializeField] private ButtonController graphicsSettings;
+
+    [SerializeField] private Dropdown resolutionDropdown;
+    private ResolutionController resolutionController;
+    private ScreenModeController screenModeController;
+    private GraphicsController graphicsController;
+
+    /// <summary>
+    /// 해상도 설정
+    /// </summary>
+    /// <param name="x"></param>
+    public void OnClickResolution(int x)
+    {
+        resolutionController.DropdownOptionChange(x);
     }
 
     /// <summary>
@@ -26,9 +160,10 @@ public class GameSettings : MonoBehaviour
     /// <param name="index"></param>
     public void OnClickScreenSetting(int index)
     {
-        if (index == currentScreenIndex) return;
-        SetActiveOnly(screenCheckmarks, index);
-        currentScreenIndex = index;
+        screenSettings.OnClickSetting(index);
+
+        bool isFullScreen = (index == 0);
+        screenModeController.SetFullScreen(isFullScreen);
     }
 
     /// <summary>
@@ -37,14 +172,21 @@ public class GameSettings : MonoBehaviour
     /// <param name="index"></param>
     public void OnClickGraphicsSetting(int index)
     {
-        if (index == currentGraphicsIndex) return;
-        SetActiveOnly(graphicsCheckmarks, index);
-        currentGraphicsIndex = index;
+        graphicsSettings.OnClickSetting(index);
+        graphicsController.SetQuality(index);
     }
 
     private void Start()
     {
-        OnClickScreenSetting(0);
-        OnClickGraphicsSetting(0);
+        screenSettings.Init(0);
+        graphicsSettings.Init(0);
+
+        resolutionController = new ResolutionController();
+        screenModeController = new ScreenModeController();
+        graphicsController = new GraphicsController();
+
+        resolutionController.resolutionDropdown = resolutionDropdown;
+        resolutionController.InitUI();
+        resolutionDropdown.onValueChanged.AddListener(resolutionController.DropdownOptionChange);
     }
 }
