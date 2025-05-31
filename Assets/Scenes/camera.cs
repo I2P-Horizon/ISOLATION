255a2576c0f8077773d30491c020f950c98a25cf
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-05-31 21:26:55 +0900

    Revert "changes"
    This reverts commit d5536a7b07881f076e9bc601125a61fd05f680ff.

--- Assets/Scenes/camera.cs
+++ Assets/Scenes/camera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scene : MonoBehaviour
+public class camera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
