ec774c9f6306430169c00e133273fa2d701fd430
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-06-01 20:24:43 +0900

    GameScene UI

--- Assets/KSW/Script/GameManager.cs
+++ Assets/KSW/Script/GameManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    #region Pause
     private GameSettings gameSettings;
-
     public GameObject pauseUI;
+
+    public Button continueButton;
+    public Button settingButton;
+    public Button exitButton;
 
     public void Pause()
     {
@@ -18,20 +23,38 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void GameSettingsUI() { gameSettings.GameSettingsUI(); }
+    private void Continue()
+    {
+        pauseUI.GetComponent<UIAnimator>().Close();
+    }
+
+    private void Settings()
+    {
+        gameSettings.gameSettingsUI.GetComponent<UIAnimator>().Show();
+    }
+
+    private void Exit()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    #endregion
 
     private void Update()
     {
         Pause();
     }
 
-    private void Awake()
-    {
-        SceneManager.LoadScene("GameSetting", LoadSceneMode.Additive);
-    }
-
     private void Start()
     {
         gameSettings = FindFirstObjectByType<GameSettings>();
+
+        continueButton.onClick.AddListener(Continue);
+        settingButton.onClick.AddListener(Settings);
+        exitButton.onClick.AddListener(Exit);
+    }
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("GameSetting", LoadSceneMode.Additive);
     }
 }
