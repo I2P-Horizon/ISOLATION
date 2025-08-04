23606b93b172a7234e7d90dd7992907873b0b28c
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-08-05 01:47:50 +0900

    최적화 6
    + 머티리얼 최적화 코드 추가

---
+++ Assets/KSW/Script/Level Design/BlockOptimizerLite.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BlockOptimizerLite : MonoBehaviour
+{
+    [Header("옵션")]
+    public bool makeStatic = true;
+    public bool enableGPUInstancing = true;
+    public bool disableShadows = false;
+
+    void OptimizeSelf()
+    {
+        if (makeStatic) gameObject.isStatic = true;
+
+        var renderer = GetComponent<MeshRenderer>();
+        if (renderer && renderer.sharedMaterial)
+        {
+            if (enableGPUInstancing && !renderer.sharedMaterial.enableInstancing)
+                renderer.sharedMaterial.enableInstancing = true;
+
+            if (disableShadows)
+            {
+                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                renderer.receiveShadows = false;
+            }
+        }
+    }
+
+    void Start()
+    {
+        OptimizeSelf();
+    }
+}
