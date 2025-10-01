dada467aa4eeb6095f6ddb3523d5cfc6d0cc33b8
SW9332 <sw933200@gmail.com>
2025-10-02 03:34:28 +0900

    시놉시스2

--- Assets/SynopsisManager.cs
+++ Assets/SynopsisManager.cs
@@ -7,10 +7,15 @@ public class SynopsisManager : MonoBehaviour
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private float duration = 5f;
 
+    [SerializeField] private GameObject player;
     [SerializeField] private Animator playerMotion;
 
     private Fade fade;
 
+    /// <summary>
+    /// 카메라 무빙 씬
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Scene1()
     {
         mainCamera.transform.position = new Vector3(-15.29f, 15f, 17.7f);
@@ -51,6 +56,10 @@ public class SynopsisManager : MonoBehaviour
         StartCoroutine(Scene2());
     }
 
+    /// <summary>
+    /// 플레이어 의자
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Scene2()
     {
         StartCoroutine(fade.FadeIn(Color.black));
@@ -60,11 +69,66 @@ public class SynopsisManager : MonoBehaviour
         StartCoroutine(Scene3());
     }
 
+    /// <summary>
+    /// 플레이어 의자에서 선글라스 올림
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Scene3()
     {
         yield return null;
         playerMotion.SetBool("Scene2", true);
+        yield return new WaitForSeconds(5f);
+
+        playerMotion.SetBool("Walking", true);
+        yield return StartCoroutine(MovePlayerForward(1.5f, 1));
+
+        playerMotion.SetBool("Idle", true);
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(Scene4());
     }
+
+    private IEnumerator Scene4()
+    {
+        yield return null;
+        mainCamera.transform.position = new Vector3(2.5f, 4.3f, 6.6f);
+        mainCamera.transform.rotation = Quaternion.Euler(3.8f, 213f, 0f);
+
+        StartCoroutine(MoveCameraForwardSmooth(0.5f, 5f));
+    }
+
+    private IEnumerator MovePlayerForward(float distance, float speed)
+    {
+        float moved = 0f;
+
+        while (moved < distance)
+        {
+            float step = speed * Time.deltaTime;
+            player.transform.position += player.transform.forward * step;
+            moved += step;
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveCameraForwardSmooth(float distance, float duration)
+    {
+        Vector3 startPos = mainCamera.transform.position;
+        Vector3 endPos = startPos + mainCamera.transform.forward * distance;
+
+        float timer = 0f;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / duration;
+            float smoothT = Mathf.SmoothStep(0f, 1f, t);
+
+            mainCamera.transform.position = Vector3.Lerp(startPos, endPos, smoothT);
+            yield return null;
+        }
+
+        // 마지막 위치 보정
+        mainCamera.transform.position = endPos;
+    }
+
 
     private void Start()
     {
